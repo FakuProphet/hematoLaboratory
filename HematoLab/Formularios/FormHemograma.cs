13d6051b385894f78f751fa96d498c142b92c0d5
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2018-10-21 15:55:12 -0300

    limpieza general

--- HematoLab/Formularios/FormHemograma.cs
+++ HematoLab/Formularios/FormHemograma.cs
@@ -1,14 +1,9 @@
 ﻿using HematoLab.Clases;
 using HematoLab.Gestores;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HematoLab.Formularios
